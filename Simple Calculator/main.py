052e6dcade8be97064f1aaffa6727edfffa39711
Arian <67290963+Ghazanfariyan-Arian@users.noreply.github.com>
2024-09-05 12:18:41 -0400

    update

---
+++ Simple Calculator/main.py
@@ -0,0 +1,5 @@
+input1 = int(input("number1: "));
+input2 = int(input("number2: "));
+
+result = input1+input2;
+print("number1 + number2 = " + str(result));
--- UIT_AutoLogin/main.py
+++ UIT_AutoLogin/main.py
@@ -4,29 +4,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
 # Setup WebDriver
-s = Service('/Users/arian/Documents/GitHub/python_projects/Automate Signin/chromedriver-mac-arm64/chromedriver')  # Update this to your ChromeDriver path
+s = Service('/Users/arian/Documents/GitHub/python_projects/UIT_AutoLogin/chromedriver-mac-arm64/chromedriver')  # Update this to your ChromeDriver path
 driver = webdriver.Chrome(service=s)
-
 # Open the registration page
-driver.get("https://arxfuture.com/MyFirstDeployment/register.html")
-
+driver.get("https://yorku.ca/csstaff/")
 # Assuming the input fields have ids 'username', 'password', and 'email'
 # Replace these with the actual IDs or modify the locator strategy (e.g., By.NAME)
-username = driver.find_element(By.ID, 'username')
+username = driver.find_element(By.ID, 'mli')
 password = driver.find_element(By.ID, 'password')
-
 # Input data
-username.send_keys("arian")
-password.send_keys("1234")
-
+username.send_keys("")
+password.send_keys("")
 # Submitting the form
 # Assuming the button has a unique tag; otherwise, you might need to use its ID or class
-submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+submit_button = driver.find_element(By.XPATH, "//input[@type='submit']")
 submit_button.click()
 
-# Optionally, add some delay to see what happens
-import time
-time.sleep(10)
 
-# Close the browser
-driver.quit()
+input("Press Enter to close the browser...")
+
